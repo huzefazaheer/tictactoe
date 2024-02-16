e0b18feef81ab1cffb5d481cf63e4325db85b24b
Huzefa <huzefazaheer1+git@gmail.com>
2024-02-16 23:27:27 +0500

    Added click functionality to gameboard

---
+++ index.js
@@ -0,0 +1,21 @@
+const gameBoard = document.querySelector(".gameboard");
+
+let turn = 0;
+function updateGameBoard(posX, posY, divNumber) {
+  let marker = "X";
+  console.log(turn % 2);
+  if (turn % 2 == 0) {
+    marker = "X";
+  } else {
+    marker = "O";
+  }
+  turn++;
+  gameBoard.childNodes[divNumber].innerText = marker;
+}
+
+for (let i = 0; i < gameBoard.childNodes.length; i++) {
+  let node = gameBoard.childNodes[i];
+  node.addEventListener("click", (e) => {
+    updateGameBoard(1, 1, i);
+  });
+}
