8213daa4c3862a852d231ce593b0664aed036541
Huzefa <huzefazaheer1+git@gmail.com>
2024-02-17 15:14:59 +0500

    Made gameboard interactive

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const gameBoard = document.querySelector(".gameboard");
 
 let turn = 0;
 function updateGameBoard(posX, posY, divNumber) {
-  let marker = "X";
-  console.log(turn % 2);
-  if (turn % 2 == 0) {
-    marker = "X";
-  } else {
-    marker = "O";
+  let div = gameBoard.childNodes[divNumber];
+  if (div.innerHTML == "") {
+    let marker = "X";
+    console.log(turn % 2);
+    if (turn % 2 == 0) {
+      marker = "X";
+    } else {
+      marker = "O";
+    }
+    turn++;
+    div.innerText = marker;
   }
-  turn++;
-  gameBoard.childNodes[divNumber].innerText = marker;
 }
 
 for (let i = 0; i < gameBoard.childNodes.length; i++) {
