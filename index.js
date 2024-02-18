dd20d3cc362a933e3a88894d2a9d38ea303b32a7
Huzefa <huzefazaheer1+git@gmail.com>
2024-02-18 13:51:07 +0500

    Wrapped gameboard in wrapper and added players instead of randomly cycling though two markers

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-const gameBoard = document.querySelector(".gameboard");
+const div_gameBoard = document.querySelector(".gameboard");
 const resultScreen = document.querySelector(".result");
 const btn_playAgain = document.querySelector("#playagain");
 
-let turn = 0;
 function makeBoard() {
-  return (board = []);
+  let fields = ["", "", "", "", "", "", "", "", ""];
+  let player1 = makePlayer("Huzefa", "X");
+  let player2 = makePlayer("Ibrahim", "O");
+
+  let turn = 0;
+
+  function getTurn() {
+    let player;
+    if (turn % 2 == 0) {
+      player = player1;
+    } else {
+      player = player2;
+    }
+    turn++;
+    return player;
+  }
+
+  function playTurn(pos) {
+    player = getTurn();
+    fields[pos] = player.marker;
+    console.log(fields);
+    document.getElementById(pos).innerText = player.marker;
+  }
+
+  return { playTurn };
+}
+
+function makePlayer(name, marker) {
+  let winCount = 0;
+  return { name, marker, winCount };
 }
 
 function clearBoard() {
-  gameBoard.childNodes.forEach((node) => {
+  div_gameBoard.childNodes.forEach((node) => {
     node.innerHTML = "";
   });
 }
@@ -21,33 +49,36 @@ function toggleResultScreen() {
   }
 }
 
-function updateGameBoard(posX, posY, divNumber) {
-  let div = gameBoard.childNodes[divNumber];
-  if (div.innerHTML == "") {
-    let marker = "x";
-    console.log(turn % 2);
-    if (turn % 2 == 0) {
-      marker = "X";
-    } else {
-      marker = "O";
-    }
-    turn++;
-    div.innerText = marker;
-  }
-  if (turn == 9) {
-    toggleResultScreen();
-  }
-}
-
-for (let i = 0; i < gameBoard.childNodes.length; i++) {
-  let node = gameBoard.childNodes[i];
-  node.addEventListener("click", (e) => {
-    updateGameBoard(1, 1, i);
-  });
-}
-
 function playAgain() {
   clearBoard();
   toggleResultScreen();
   turn = 0;
+}
+
+let gameBoard = makeBoard();
+
+// function updatediv_GameBoard(posX, posY, divNumber) {
+//   let div = div_gameBoard.childNodes[divNumber];
+//   if (div.innerHTML == "") {
+//     let marker = "x";
+//     console.log(turn % 2);
+//     if (turn % 2 == 0) {
+//       marker = "X";
+//     } else {
+//       marker = "O";
+//     }
+//     turn++;
+//     div.innerText = marker;
+//   }
+//   if (turn == 9) {
+//     toggleResultScreen();
+//   }
+// }
+
+for (let i = 0; i < div_gameBoard.childNodes.length; i++) {
+  let node = div_gameBoard.childNodes[i];
+  node.addEventListener("click", (e) => {
+    // updatediv_GameBoard(1, 1, i);
+    gameBoard.playTurn(node.id);
+  });
 }
